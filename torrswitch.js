b247cf671c0d6e67faa4548519ca98d6c108c05d
4irika <exstazikk@gmail.com>
2026-05-14 04:15:36 +0300

    Implement TorrServer switch with settings menu

--- torrswitch.js
+++ torrswitch.js
@@ -1,3 +1,79 @@
 (() => {
-    Lampa.Noty.show('Расширение загружено');
+    const KEY_LIST = 'torrswitch_list';
+    const KEY_ACTIVE = 'torrswitch_active';
+
+    function getList() {
+        let list = Lampa.Storage.get(KEY_LIST, []);
+        return Array.isArray(list) ? list : [];
+    }
+
+    function saveList(list) {
+        Lampa.Storage.set(KEY_LIST, list);
+    }
+
+    function getActive() {
+        return Lampa.Storage.get(KEY_ACTIVE, '');
+    }
+
+    function setActive(url) {
+        Lampa.Storage.set(KEY_ACTIVE, url);
+    }
+
+    function openMenu() {
+        let list = getList();
+        let active = getActive();
+
+        let modal = new Lampa.Modal({
+            title: 'Переключатель TorrServer',
+            onBack: () => Lampa.Controller.toggle('settings')
+        });
+
+        let body = modal.body();
+
+        body.append('<div class="settings-param-title">Список серверов</div>');
+
+        list.forEach((url, i) => {
+            let row = $('<div class="settings-param"></div>');
+            let input = $('<input class="settings-input" placeholder="http://IP:8090/">');
+            let btn = $('<div class="settings-param__name" style="color:#00a1ff;margin-top:5px">Сделать активным</div>');
+
+            input.val(url);
+
+            input.on('change', () => {
+                list[i] = input.val().trim();
+                saveList(list);
+            });
+
+            btn.on('click', () => {
+                setActive(url);
+                Lampa.Noty.show('Активный сервер: ' + url);
+            });
+
+            row.append(input);
+            row.append(btn);
+            body.append(row);
+        });
+
+        let addBtn = $('<div class="settings-param settings-param--button"><div class="settings-param__name">Добавить сервер</div></div>');
+        addBtn.on('click', () => {
+            list.push('');
+            saveList(list);
+            modal.update();
+        });
+
+        body.append(addBtn);
+
+        body.append('<div class="settings-param-title" style="margin-top:20px">Текущий активный сервер</div>');
+        body.append('<div class="settings-param"><div class="settings-param__name">' + (active || 'Не выбран') + '</div></div>');
+
+        modal.create();
+    }
+
+    // Добавляем пункт в настройки
+    Lampa.SettingsApi.addComponent({
+        title: 'Переключатель TorrServer',
+        onRender: openMenu
+    });
+
+    Lampa.Noty.show('Переключатель TorrServer загружен');
 })();
