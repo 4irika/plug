0370f69fdfd34d1c98d4835e92a54c966f711c28
4irika <exstazikk@gmail.com>
2026-05-14 03:07:27 +0300

    Refactor TorrServer extra plugin for better structure

--- torrserver-extra.js
+++ torrserver-extra.js
@@ -1,58 +1,76 @@
+/*
+    Lampa Plugin: Дополнительные TorrServer
+    Version: 1.0.1
+*/
+
 (function () {
-    const STORAGE_KEY = 'torrserver_extra_urls';
+    'use strict';
+
+    var STORAGE_KEY = 'torrserver_extra_urls';
 
     function getStoredUrls() {
-        return Lampa.Storage.get(STORAGE_KEY, []);
+        var urls = Lampa.Storage.get(STORAGE_KEY, []);
+        if (!Array.isArray(urls)) urls = [];
+        return urls;
     }
 
     function saveUrls(urls) {
         Lampa.Storage.set(STORAGE_KEY, urls);
     }
 
-    Lampa.Plugin.create({
-        title: 'Доп. TorrServer',
-        icon: 'icon',
-        onStart: function () {
-            const urls = getStoredUrls();
+    function renderSettings() {
+        var urls = getStoredUrls();
 
-            Lampa.SettingsApi.addComponent({
-                component: 'torrserver_extra',
-                name: 'Дополнительные TorrServer',
-                icon: 'torrent',
-                onRender: function (body) {
-                    body.append('<div class="settings-param-title">Дополнительные ссылки на TorrServer</div>');
+        var settings = new Lampa.Settings({
+            title: 'Дополнительные TorrServer',
+            onBack: function () {
+                Lampa.Controller.toggle('settings');
+            }
+        });
 
-                    urls.forEach((url, index) => {
-                        const input = $('<input class="settings-input" placeholder="http://IP:8090/" />');
-                        input.val(url);
+        var body = settings.body();
 
-                        input.on('change', function () {
-                            urls[index] = $(this).val();
-                            saveUrls(urls);
-                        });
+        var info = $('<div class="settings-param-title">Дополнительные ссылки на TorrServer</div>');
+        body.append(info);
 
-                        body.append(input);
-                    });
+        urls.forEach(function (url, index) {
+            var item = $('<div class="settings-param"></div>');
+            var input = $('<input class="settings-input" placeholder="http://IP:8090/" />');
 
-                    const addBtn = $('<div class="settings-param settings-param--button"><div class="settings-param__name">Добавить поле</div></div>');
-                    addBtn.on('click', function () {
-                        urls.push('');
-                        saveUrls(urls);
-                        Lampa.Settings.update();
-                    });
+            input.val(url);
 
-                    body.append(addBtn);
-                }
+            input.on('change', function () {
+                urls[index] = $(this).val().trim();
+                saveUrls(urls);
             });
 
-            Lampa.SettingsApi.addParam({
-                component: 'torrserver_extra',
-                param: {
-                    name: 'info',
-                    type: 'text',
-                    value: 'Здесь вы можете добавить дополнительные ссылки на TorrServer'
-                }
-            });
-        }
-    });
+            item.append(input);
+            body.append(item);
+        });
+
+        var addBtn = $('<div class="settings-param settings-param--button"><div class="settings-param__name">Добавить поле</div></div>');
+
+        addBtn.on('click', function () {
+            urls.push('');
+            saveUrls(urls);
+            settings.update();
+        });
+
+        body.append(addBtn);
+
+        settings.create();
+    }
+
+    function startPlugin() {
+        // Добавляем пункт в настройки
+        Lampa.Settings.add({
+            title: 'Дополнительные TorrServer',
+            group: 'torrserver',
+            onSelect: function () {
+                renderSettings();
+            }
+        });
+    }
+
+    Lampa.Plugin.create('torrserver_extra', startPlugin, 'Доп. TorrServer', 'torrent');
 })();
