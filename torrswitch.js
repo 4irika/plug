d1dabd585a1d30384802e2cc095fe88b34b96f48
4irika <exstazikk@gmail.com>
2026-05-14 04:27:22 +0300

    Refactor to use class for TorrSwitchComponent

--- torrswitch.js
+++ torrswitch.js
@@ -19,56 +19,60 @@
         Lampa.Storage.set(KEY_ACTIVE, url);
     }
 
-    function Component() {
-        let list = getList();
-        let active = getActive();
+    class TorrSwitchComponent {
+        constructor() {
+            this.html = $('<div class="settings-container"></div>');
+        }
 
-        let html = $('<div class="settings-container"></div>');
+        render(body) {
+            let list = getList();
+            let active = getActive();
 
-        html.append('<div class="settings-param-title">Список серверов</div>');
+            this.html.empty();
 
-        list.forEach((url, i) => {
-            let row = $('<div class="settings-param"></div>');
-            let input = $('<input class="settings-input" placeholder="http://IP:8090/">');
-            let btn = $('<div class="settings-param__name" style="color:#00a1ff;margin-top:5px">Сделать активным</div>');
+            this.html.append('<div class="settings-param-title">Список серверов</div>');
 
-            input.val(url);
+            list.forEach((url, i) => {
+                let row = $('<div class="settings-param"></div>');
+                let input = $('<input class="settings-input" placeholder="http://IP:8090/">');
+                let btn = $('<div class="settings-param__name" style="color:#00a1ff;margin-top:5px">Сделать активным</div>');
 
-            input.on('change', () => {
-                list[i] = input.val().trim();
+                input.val(url);
+
+                input.on('change', () => {
+                    list[i] = input.val().trim();
+                    saveList(list);
+                });
+
+                btn.on('click', () => {
+                    setActive(url);
+                    Lampa.Noty.show('Активный сервер: ' + url);
+                });
+
+                row.append(input);
+                row.append(btn);
+                this.html.append(row);
+            });
+
+            let addBtn = $('<div class="settings-param settings-param--button"><div class="settings-param__name">Добавить сервер</div></div>');
+            addBtn.on('click', () => {
+                list.push('');
                 saveList(list);
+                Lampa.SettingsApi.update();
             });
 
-            btn.on('click', () => {
-                setActive(url);
-                Lampa.Noty.show('Активный сервер: ' + url);
-            });
+            this.html.append(addBtn);
 
-            row.append(input);
-            row.append(btn);
-            html.append(row);
-        });
+            this.html.append('<div class="settings-param-title" style="margin-top:20px">Текущий активный сервер</div>');
+            this.html.append('<div class="settings-param"><div class="settings-param__name">' + (active || 'Не выбран') + '</div></div>');
 
-        let addBtn = $('<div class="settings-param settings-param--button"><div class="settings-param__name">Добавить сервер</div></div>');
-        addBtn.on('click', () => {
-            list.push('');
-            saveList(list);
-            Lampa.SettingsApi.update();
-        });
-
-        html.append(addBtn);
-
-        html.append('<div class="settings-param-title" style="margin-top:20px">Текущий активный сервер</div>');
-        html.append('<div class="settings-param"><div class="settings-param__name">' + (active || 'Не выбран') + '</div></div>');
-
-        this.render = function (body) {
-            body.append(html);
-        };
+            body.append(this.html);
+        }
     }
 
     Lampa.SettingsApi.addComponent({
         name: 'Переключатель TorrServer',
-        component: Component
+        component: TorrSwitchComponent
     });
 
     Lampa.Noty.show('Переключатель TorrServer загружен');
