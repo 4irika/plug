b452931f401a49840c054278c7919af0534c4449
4irika <exstazikk@gmail.com>
2026-05-14 03:16:25 +0300

    Refactor TorrServer plugin for improved functionality

--- torrserver-extra.js
+++ torrserver-extra.js
@@ -1,76 +1,102 @@
 /*
-    Lampa Plugin: Дополнительные TorrServer
-    Version: 1.0.1
+    Lampa Plugin: TorrServer Switcher
+    Version: 1.2.0
+    Author: 4irika + Copilot
 */
 
 (function () {
     'use strict';
 
-    var STORAGE_KEY = 'torrserver_extra_urls';
+    const STORAGE_LIST = 'torrserver_extra_urls';
+    const STORAGE_ACTIVE = 'torrserver_active_url';
 
-    function getStoredUrls() {
-        var urls = Lampa.Storage.get(STORAGE_KEY, []);
-        if (!Array.isArray(urls)) urls = [];
-        return urls;
+    function getList() {
+        let list = Lampa.Storage.get(STORAGE_LIST, []);
+        if (!Array.isArray(list)) list = [];
+        return list;
     }
 
-    function saveUrls(urls) {
-        Lampa.Storage.set(STORAGE_KEY, urls);
+    function saveList(list) {
+        Lampa.Storage.set(STORAGE_LIST, list);
     }
 
-    function renderSettings() {
-        var urls = getStoredUrls();
+    function getActive() {
+        return Lampa.Storage.get(STORAGE_ACTIVE, '');
+    }
 
-        var settings = new Lampa.Settings({
-            title: 'Дополнительные TorrServer',
+    function setActive(url) {
+        Lampa.Storage.set(STORAGE_ACTIVE, url);
+    }
+
+    function openSettings() {
+        const list = getList();
+        const active = getActive();
+
+        const settings = new Lampa.Settings({
+            title: 'Переключатель TorrServer',
             onBack: function () {
                 Lampa.Controller.toggle('settings');
             }
         });
 
-        var body = settings.body();
+        const body = settings.body();
 
-        var info = $('<div class="settings-param-title">Дополнительные ссылки на TorrServer</div>');
-        body.append(info);
+        body.append('<div class="settings-param-title">Список TorrServer</div>');
 
-        urls.forEach(function (url, index) {
-            var item = $('<div class="settings-param"></div>');
-            var input = $('<input class="settings-input" placeholder="http://IP:8090/" />');
+        list.forEach((url, index) => {
+            const item = $('<div class="settings-param"></div>');
+            const input = $('<input class="settings-input" placeholder="http://IP:8090/" />');
+            const selectBtn = $('<div class="settings-param__name" style="margin-top:5px;cursor:pointer;color:#00a1ff">Сделать активным</div>');
 
             input.val(url);
 
             input.on('change', function () {
-                urls[index] = $(this).val().trim();
-                saveUrls(urls);
+                list[index] = $(this).val().trim();
+                saveList(list);
+            });
+
+            selectBtn.on('click', function () {
+                setActive(url);
+                Lampa.Noty.show('Активный сервер: ' + url);
             });
 
             item.append(input);
+            item.append(selectBtn);
             body.append(item);
         });
 
-        var addBtn = $('<div class="settings-param settings-param--button"><div class="settings-param__name">Добавить поле</div></div>');
-
+        const addBtn = $('<div class="settings-param settings-param--button"><div class="settings-param__name">Добавить сервер</div></div>');
         addBtn.on('click', function () {
-            urls.push('');
-            saveUrls(urls);
+            list.push('');
+            saveList(list);
             settings.update();
         });
 
         body.append(addBtn);
 
+        const activeBlock = $('<div class="settings-param-title" style="margin-top:20px">Текущий активный сервер</div>');
+        const activeValue = $('<div class="settings-param"><div class="settings-param__name">' + (active || 'Не выбран') + '</div></div>');
+
+        body.append(activeBlock);
+        body.append(activeValue);
+
         settings.create();
     }
 
     function startPlugin() {
-        // Добавляем пункт в настройки
         Lampa.Settings.add({
-            title: 'Дополнительные TorrServer',
+            title: 'Переключатель TorrServer',
             group: 'torrserver',
             onSelect: function () {
-                renderSettings();
+                openSettings();
             }
         });
     }
 
-    Lampa.Plugin.create('torrserver_extra', startPlugin, 'Доп. TorrServer', 'torrent');
+    Lampa.Plugin.create(
+        'torrserver_switcher',
+        startPlugin,
+        'Переключатель TorrServer',
+        'torrent'
+    );
 })();
