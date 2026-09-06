88d692fdd1b2cebbae9eed280401bfd192df5aa4
4irika <exstazikk@gmail.com>
2026-09-06 16:10:24 +0300

    Update no.js

--- no.js
+++ no.js
@@ -3,7 +3,7 @@
         if (window.Lampa && Lampa.Player && Lampa.Player.play) {
             var originalPlay = Lampa.Player.play;
             Lampa.Player.play = function (object) {
-                object.iptv = true;
+            
 
                 if (object.vast_url) delete object.vast_url;
                 if (object.vast_msg) delete object.vast_msg;
